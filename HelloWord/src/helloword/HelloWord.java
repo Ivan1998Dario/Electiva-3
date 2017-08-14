eaaef71fd5cf2ad2d1051b7f798cd06fe35ca629
ESTUDIANTE <ESTUDIANTE@S4-15>
2017-08-14 06:36:08 -0500

    probando el repositorio GIT.JB con el programa de HelloWorld

---
+++ HelloWord/src/helloword/HelloWord.java
@@ -0,0 +1,18 @@
+
+package helloword;
+
+/**
+ *
+ * @author Ivan
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+    
+}
+
